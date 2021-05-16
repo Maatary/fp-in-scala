d972c709bd664a08e4a3eba136d55ded52344fab
Maatari Okouya <d.okouya@elsevier.com>
2021-05-16 22:35:11 +0100

    Parallel Queries with doobie

--- src/main/scala/dobbie/DoobieStarterApp.scala
+++ src/main/scala/dobbie/DoobieStarterApp.scala
@@ -1,10 +1,15 @@
 package dobbie
 
+
+import scala.concurrent.Future
+
+
 object DoobieStarterApp extends App {
 
   import doobie._
   import doobie.implicits._
-  import cats.effect.IO
+  import cats.effect._
+  import cats.syntax.all._
   import cats.effect.unsafe.implicits.global
 
   val transactor = Transactor.fromDriverManager[IO](
@@ -14,12 +19,36 @@ object DoobieStarterApp extends App {
     "ro_pse"
     )
 
+  case class PropertyType(id: Int, `type`: String)
+  case class ObjectType(id: Int, `type`: String)
 
-  val q = sql"""select "ID", "sPropertyName" from "PropTypes"""".query[(Int, String)].option
+  val q1 = sql"""select "ID", "sPropertyName" from "PropTypes"""".query[PropertyType].accumulate[List]
+
+  val q2 = sql"""select "ID", "sPropertyName" from "PropTypes"""".query[ObjectType].accumulate[List]
 
 
-  val res = q.transact(transactor).unsafeRunSync()
 
-  println(res)
+ val schemaQueries =  (q1.transact(transactor), q2.transact(transactor)).parTupled
+
+ val prog = for {
+
+    res <- schemaQueries
+    (properties, objects) = res
+
+    _ <- IO { println(s"properties: $properties")}
+
+    _ <- IO { println(s"Objects: $objects") }
+
+  } yield ()
+
+
+
+  prog.unsafeRunSync()
+
+  println(List("hello", "World", "Maat").mkString("select * from (\n\n" , "\n Union All \n", " \n)" ))
+
+
+
+
 
 }
